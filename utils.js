2ede026552150a966b09ba73c6d64de8aa931376
AceZephyr <pokefan107@gmail.com>
2018-06-12 13:52:23 -0400

    june 20 3

--- utils.js
+++ utils.js
@@ -12,26 +12,35 @@ function getBySpecialId(id) {
 				if (koroksArr[i].id === id) {
 					var iStr = i + "";
 					var zeroes = "0".repeat(Math.ceil(Math.log10(koroksArr.length)));
-					var realid = "Korok Seeds@" + (zeroes.substr(0, zeroes.length - iStr.length) + iStr);
+					return "Korok Seeds@" + (zeroes.substr(0, zeroes.length - iStr.length) + iStr);
 				}
 			}
-		} else if (id.endsWith("Shrine")) {
+		} else if (id.endsWith(" Shrine")) {
 			var shrinesArr = DATA.Shrines.locations;
 			for (var i = 0; i < shrinesArr.length; i++) {
 				if (shrinesArr[i].id === id) {
 					var iStr = i + "";
-					var zeroes = "0".repeat(Math.ceil(Math.log10(koroksArr.length)));
-					var realid = "Shrines@" + (zeroes.substr(0, zeroes.length - iStr.length) + iStr);
+					var zeroes = "0".repeat(Math.ceil(Math.log10(shrinesArr.length)));
+					return "Shrines@" + (zeroes.substr(0, zeroes.length - iStr.length) + iStr);
 				}
 			}
 			var dlcShrinesArr = DATA["Champion`s Ballad Shrines"].locations;
 			for (var i = 0; i < dlcShrinesArr.length; i++) {
-				if (shrinesArr[i].id === id) {
+				if (dlcShrinesArr[i].id === id) {
 					var iStr = i + "";
-					var zeroes = "0".repeat(Math.ceil(Math.log10(koroksArr.length)));
-					var realid = "Champion`s Ballad Shrines@" + (zeroes.substr(0, zeroes.length - iStr.length) + iStr);
+					var zeroes = "0".repeat(Math.ceil(Math.log10(dlcShrinesArr.length)));
+					return "Champion`s Ballad Shrines@" + (zeroes.substr(0, zeroes.length - iStr.length) + iStr);
 				}
 			}
-		}
-	}
+		} else if (id.endsWith(" Tower")) {
+			var towersArr = DATA.Towers.locations;
+			for (var i = 0; i < towersArr.length; i++) {
+				if (towersArr[i].id === id) {
+					var iStr = i + "";
+					var zeroes = "0".repeat(Math.ceil(Math.log10(towersArr.length)));
+					return "Towers@" + (zeroes.substr(0, zeroes.length - iStr.length) + iStr);
+				}
+			}
+		} else return id;
+	} else return id;
 }
